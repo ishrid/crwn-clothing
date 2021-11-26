936604943074fad7ebd5c14a60b833455cd8d5d6
ishrid <39648826+ishrid@users.noreply.github.com>
2021-11-26 19:20:21 +0100

    aggiunta del nav bar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.comp";
 import ShopPage from "./pages/shop/shop.comp";
+import Header from "./components/header/header.comp";
  
 function App() {
   return (
     <div> 
+      <Header />
 <Switch>
       <Route exact path='/' component={HomePage} />
       <Route  path='/shop' component={ShopPage} />
